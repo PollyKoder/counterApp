bcb978538a097cd952cf55e21f82adcaf50d2848
Polly <riviera.koder@gmail.com>
2023-10-02 14:33:45 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const plussButton = document.querySelector("#bPlus");
+console.log(plussButton);
+const minusButton = document.querySelector("#bMinus");
+console.log(minusButton);
+//
+//Variabler som kan reknes med;
+let count = 0;
+
+// funksjon;
+function increment() {
+     count + 1;
+     console.log(count);
+}
+
+increment();
+increment();
