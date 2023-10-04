9da2bd1674807da8d4f96e7642f85427560ba156
Polly <riviera.koder@gmail.com>
2023-10-04 14:47:28 +0200

    edit5

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ukens oppgave vil være å sette opp en applikasjon som skal øke og minke et tall.
+Vi kommer til å ta i bruk:
+- Variabler
+- Datatyper
+- Funksjoner
+- DOM Manipulasjon
+- For å lage denne applikasjonen.
+
+# Krav:
+- Fullverdig html dokument (tenk semantisk, tenk universal utforming)
+- Fullverdig stylet nettside i css (tenk responsive, tenk fargepaletter)
+- Fungerende javascript som øker og senker et tall
+- Tallet skal øke og minke visuelt på nettsiden
+
+Innlevering:
+- Github repository lenke
+- Netlity nettside lenke
+
+# Due Oct 6
+
+
+#E Egne notater:
+- 
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const counterValue = document.querySelector("#counterValueID");
+console.log(counterValue);
 const plussButton = document.querySelector("#bPlus");
 console.log(plussButton);
 const minusButton = document.querySelector("#bMinus");
@@ -12,25 +14,14 @@ function increment() {
      console.log(count);
 }
 
-increment();
-increment();
-increment();
-increment();
-increment();
-increment();
-increment();
-increment();
+
 
 function decrement() {
     count = count - 1;
     console.log(count);
 }
 
-decrement();
-decrement();
-decrement();
-decrement();
-decrement();
-decrement();
-decrement();
-decrement();
+//Velger å bruke addEventLister, da den virker mer intuitiv for meg 
+// - og jeg liker at jeg da kan legge ved flere funksjoner senere//
+plussButton.addEventListener("click", increment);
+minusButton.addEventListener("click", decrement);
