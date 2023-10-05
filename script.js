1059db54a6a731d521ddf1e62a7a56de9b73dae8
Polly <riviera.koder@gmail.com>
2023-10-05 11:10:07 +0200

    edit9

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const plussButton = document.querySelector("#bPlus");
 console.log(plussButton);
 const minusButton = document.querySelector("#bMinus");
 console.log(minusButton);
+const bReset = document.querySelector("#reset");
+console.log(bReset);
 //
 //Variabler som kan reknes med;
 let count = 0;
@@ -12,14 +14,23 @@ let count = 0;
 function increment() {
   count = count + 1;
   console.log(count);
+  counterValue.textContent = count;
 }
 
 function decrement() {
   count = count - 1;
   console.log(count);
+  counterValue.textContent = count;
 }
 
 //Velger å bruke addEventLister, da den virker mer intuitiv for meg
 // - og jeg liker at jeg da kan legge ved flere funksjoner senere//
 plussButton.addEventListener("click", increment);
 minusButton.addEventListener("click", decrement);
+
+bReset.addEventListener("click", reset);
+
+function reset() {
+    count = 0;
+    counterValue.textContent = count;
+}
